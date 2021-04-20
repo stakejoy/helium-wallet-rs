018cd825ab8dcf3cb1885989d0784461114dcee5
lthiery <thiery.louis@gmail.com>
2021-04-20 11:04:43 -0700

    implement sweep

--- src/cmd/pay.rs
+++ src/cmd/pay.rs
@@ -1,10 +1,13 @@
 use crate::{
     cmd::*,
     keypair::PublicKey,
-    result::Result,
+    result::{anyhow, Result},
     traits::{TxnEnvelope, TxnFee, TxnSign, B64},
 };
-use helium_api::accounts;
+use helium_api::{
+    accounts::{self, Account},
+    oracle,
+};
 use prettytable::Table;
 use serde_json::json;
 use std::str::FromStr;
@@ -28,6 +31,12 @@ pub struct Cmd {
     #[structopt(long)]
     nonce: Option<u64>,
 
+    /// Only impacts sweep payouts. Sets how many minutes in the future
+    /// oracle prices should be considered for. Set to 0 for "optimistic"
+    /// submission with current price.
+    #[structopt(long, default_value = "120")]
+    oracle_window: u64,
+
     /// Manually set the DC fee to pay for the transaction
     #[structopt(long)]
     fee: Option<u64>,
@@ -43,16 +52,32 @@ impl Cmd {
         let wallet = load_wallet(opts.files)?;
 
         let client = Client::new_with_base_url(api_url(wallet.public_key.network));
-
         let keypair = wallet.decrypt(password.as_bytes())?;
+        let account = accounts::get(&client, &keypair.public_key().to_string()).await?;
+
+        let mut sweep_destination = None;
+        let mut pay_total = 0;
 
         let payments: Vec<Payment> = self
             .payees
             .iter()
-            .map(|p| Payment {
-                payee: p.address.to_vec(),
-                amount: u64::from(p.amount),
-                memo: p.memo,
+            .map(|p| {
+                let amount = if let Amount::Hnt(amount) = p.amount {
+                    let amount = u64::from(amount);
+                    pay_total += amount;
+                    amount
+                } else if sweep_destination.is_none() {
+                    sweep_destination = Some(p.address.to_vec());
+                    0
+                } else {
+                    panic!("Cannot sweep to two addresses in the same transaction!")
+                };
+
+                Payment {
+                    payee: p.address.to_vec(),
+                    amount,
+                    memo: p.memo,
+                }
             })
             .collect();
 
@@ -63,7 +88,6 @@ impl Cmd {
             nonce: if let Some(nonce) = self.nonce {
                 nonce
             } else {
-                let account = accounts::get(&client, &keypair.public_key().to_string()).await?;
                 account.speculative_nonce + 1
             },
             signature: Vec::new(),
@@ -72,7 +96,43 @@ impl Cmd {
         txn.fee = if let Some(fee) = self.fee {
             fee
         } else {
-            txn.txn_fee(&get_txn_fees(&client).await?)?
+            match sweep_destination {
+                // if there is no sweep destination, txn fees are simply calculated
+                None => txn.txn_fee(&get_txn_fees(&client).await?)?,
+                // if there is a sweep destination, the txn fees are iteratively determined
+                // since the amount being swept affects the fee (protobuf encoding size changes)
+                Some(sweep_destination) => {
+                    let mut fee = txn.txn_fee(&get_txn_fees(&client).await?)?;
+                    loop {
+                        // sweep amount is remaining HNT after accounting for txn fees
+                        let sweep_amount = calculate_remaining_hnt(
+                            &client,
+                            &account,
+                            &pay_total,
+                            &fee,
+                            &self.oracle_window,
+                        )
+                        .await?;
+                        // update the txn with the amount for the sweep payee
+                        for payment in &mut txn.payments {
+                            if payment.payee == sweep_destination {
+                                payment.amount = sweep_amount;
+                            }
+                        }
+
+                        // calculate fee based on the new txn size
+                        let new_fee = txn.txn_fee(&get_txn_fees(&client).await?)?;
+
+                        // if the fee matches, we are done iterating
+                        if new_fee == fee {
+                            break;
+                        } else {
+                            fee = new_fee;
+                        }
+                    }
+                    fee
+                }
+            }
         };
         txn.signature = txn.sign(&keypair)?;
 
@@ -132,8 +192,26 @@ fn print_txn(
 #[derive(Debug)]
 pub struct Payee {
     address: PublicKey,
-    amount: Hnt,
+    amount: Amount,
     memo: u64,
+}
+
+#[derive(Debug)]
+enum Amount {
+    Hnt(Hnt),
+    Sweep,
+}
+
+impl std::str::FromStr for Amount {
+    type Err = Error;
+
+    fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
+        Ok(if s == "sweep" {
+            Amount::Sweep
+        } else {
+            Amount::Hnt(Hnt::from_str(s)?)
+        })
+    }
 }
 
 impl FromStr for Payee {
@@ -170,6 +248,74 @@ impl FromStr for Payee {
             })
         } else {
             panic!("Invalid command syntax. Check --help for more information")
+        }
+    }
+}
+
+async fn calculate_remaining_hnt(
+    client: &helium_api::Client,
+    account: &Account,
+    pay_total: &u64,
+    fee: &u64,
+    oracle_window: &u64,
+) -> Result<u64> {
+    use rust_decimal::{prelude::*, Decimal};
+    use std::time::{SystemTime, UNIX_EPOCH};
+
+    let hnt_balance = u64::from(account.balance);
+    // if account has the DCs for the charge,
+    // the sweep is simply the remaining balance after payment to others
+    if &account.dc_balance > fee {
+        Ok(hnt_balance - pay_total)
+    }
+    // otherwise, we need to leave enough HNT to pay the txn fee via implicit burn
+    else {
+        // if window == 0, simply return the current oracle price
+        let oracle_price = if *oracle_window == 0 {
+            oracle::prices::current(&client).await?.price
+            // else, use the oracle_window, given in minutes to select max price
+        } else {
+            let now = SystemTime::now().duration_since(UNIX_EPOCH)?;
+            let mut oracle_prices = oracle::predictions(&client).await?;
+            // filter down predictions that are not in window
+            oracle_prices.retain(|prediction| {
+                let prediction_time = prediction.time as u64;
+                // sometimes API may be lagging real time, so if prediction is already passed
+                // retain this value
+                if prediction_time < now.as_secs() {
+                    true
+                } else {
+                    // true if prediction time is within window
+                    prediction_time - now.as_secs() < oracle_window * 60
+                }
+            });
+
+            // take max of all predictions
+            oracle_prices
+                .iter()
+                .fold(oracle::prices::current(&client).await?.price, |max, x| {
+                    if max.get_decimal() < x.price.get_decimal() {
+                        x.price
+                    } else {
+                        max
+                    }
+                })
+        };
+        match Decimal::from_u64(*fee) {
+            Some(fee) => {
+                // simple decimal division tells you the amount of HNT needed
+                let mut hnt_needed = fee / oracle_price.get_decimal();
+                // fee was given in DC, which is $ 10^-5
+                // HNT is expresed in 10^8 bones
+                // so scale by 3 to get implicit burn fee in bones
+                hnt_needed.set_scale(hnt_needed.scale() - 3)?;
+                // ceil rounds up for us and change into u64 for txn building
+                match hnt_needed.ceil().to_u64() {
+                    Some(bones_needed) => Ok(hnt_balance - pay_total - bones_needed),
+                    None => Err(anyhow!("Failed to cast bones_needed into u64")),
+                }
+            }
+            None => Err(anyhow!("Failed to parse fee as Decimal")),
         }
     }
 }
